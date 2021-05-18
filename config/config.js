fa2a2bad34ae63f034a4d363ceedbde2af620d57
Luc-Govaarts <lucgovaarts@hotmail.com>
2021-05-18 11:32:27 +0200

    config, constants and file structure

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+require('dotenv').config()
+
+module.exports = {
+    development: {
+      url: process.env.DATABASE_URL,
+      dialect: "postgres",
+      operatorsAliases: "0"
   },
   "test": {
     "username": "root",
