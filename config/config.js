f31c72c7fadd39a21ada28d1603dbcfec7ca7bbe
Luc-Govaarts <lucgovaarts@hotmail.com>
2021-05-18 15:22:15 +0200

    fixed new  bug in connecting to pg db

--- config/config.js
+++ config/config.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
     dialect: "postgres",
     operatorsAliases: "0"
-  },
+  }
 }
