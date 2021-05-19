5539cae31cf129ec4d5f57b9982d81705449f99c
Luc-Govaarts <lucgovaarts@hotmail.com>
2021-05-19 15:17:31 +0200

    incrporated verification code in signup route + mail

--- nodemailer/mail.js
+++ nodemailer/mail.js
@@ -1,9 +1,7 @@
 const config = require('./config')
 
-const signupVerification = async (email, name) => {
-	const verificationCode = Math.floor(100000 + Math.random() * 900000) // genrating a random 6 digit integer between 100.000 and 999.999
-	
-    
+const signupVerification = async (email, name, verificationCode) => {
+	    
     try {
 		await config.transporter.sendMail({
 			from: '"Tinus form The ToDo App" <tinustodoapp@gmail.com>', // sender address
@@ -11,7 +9,7 @@ const signupVerification = async (email, name) => {
 			subject: 'Verify your email adress', // Subject line
 			html: `<div style="  margin: auto; width: 70%; padding: 20px;">
                         <p>Hi ${name}, <br><br>
-                            Thank you for signing up fot the Tinus To Do App. <br>
+                            Thank you for signing up for the Tinus To Do App. <br>
                             You are almost ready to start using the app. <br>
                             Before you can, please verify your email adress. <br>
                             Click the link below and enter the verification code.<br><br></p>
--- routers/auth.js
+++ routers/auth.js
@@ -45,20 +45,25 @@ router.post('/signup', async (req, res) => {
 			.send({ message: 'Please provide an email, password, a name' })
 	} else if (VERIFICATION_CODE !== code) {
 		return res.status(400).send({ message: 'invalid verification key' })
-	}
+  }
+  
+  // genrating a random 6 digit integer between 100.000 and 999.999
 
+  const verificationCode = Math.floor(100000 + Math.random() * 900000) 
+  
 	try {
 		const newUser = await User.create({
 			email,
 			password: bcrypt.hashSync(password, SALT_ROUNDS),
-			name,
+      name,
+      verificationCode
 		})
 
-		delete newUser.dataValues['password'] // don't send back the password hash
+		delete newUser.dataValues['password', 'verificationCode'] // don't send back the password hash
 
 		const token = toJWT({ userId: newUser.id })
 
-		mail.signupVerification(email, name)
+		mail.signupVerification(email, name, verificationCode)
 
 		res.status(201).json({ token, ...newUser.dataValues })
 	} catch (error) {
