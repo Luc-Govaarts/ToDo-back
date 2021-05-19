de028e8a0bc6bf1a4d54682e7272589b031dd4e1
Luc-Govaarts <lucgovaarts@hotmail.com>
2021-05-19 15:53:15 +0200

    fixed safety issue, not sending back hashed values

--- routers/auth.js
+++ routers/auth.js
@@ -23,9 +23,12 @@ router.post('/login', async (req, res, next) => {
 			return res.status(400).send({
 				message: 'User with that email not found or password incorrect',
 			})
-		}
+    }
+    
+    // don't send back the password hash or verification code hash
+    delete user.dataValues['password']
+    delete user.dataValues['verificationCode']
 
-		delete user.dataValues['password'] // don't send back the password hash
 		const token = toJWT({ userId: user.id })
 		return res.status(200).send({ token, ...user.dataValues })
 	} catch (error) {
@@ -57,9 +60,11 @@ router.post('/signup', async (req, res) => {
 			password: bcrypt.hashSync(password, SALT_ROUNDS),
       name,
       verificationCode: bcrypt.hashSync(verificationCode, SALT_ROUNDS)
-		})
-
-		delete newUser.dataValues['password', 'verificationCode'] // don't send back the password hash
+    })
+    
+    // don't send back the password hash or verification code hash
+    delete user.dataValues['password']
+    delete user.dataValues['verificationCode']
 
 		const token = toJWT({ userId: newUser.id })
 
